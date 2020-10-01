03d7a931e640196ec703d34668067789ae59f1ca
YazanMohsen008 <YazanMohsen008@gmail.com>
2020-10-01 14:33:03 +0300

    project init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(initialRoute: 'register', routes: {
+    'register': (context) {
+      return Container();
+    },
+    'home': (context) {
+      return Container();
+    },
+    'order': (context) {
+      return Container();
+    },
+  }));
+}
